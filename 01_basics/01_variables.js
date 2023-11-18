4aea49a058d0859372932d88d209e71626e45c40
Abhisek sahoo <126792721+abhisek7154@users.noreply.github.com>
2023-11-18 10:18:42 +0000

    learned variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+//const => value cannot change
+let accountEmail = "abhisek097@gmail.com"
+var accountPassoword = "12345"
+accountCity = "Odisha"
+
+//accountId = 2 // NOT allowed
+
+accountEmail = "hc@hc.com"
+accountPassoword = "212121212"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassoword, accountCity])
