e3df1833fdec530660a572323674dac36a5921fc
Abhisek sahoo <126792721+abhisek7154@users.noreply.github.com>
2023-11-27 12:51:59 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // Treat all JS code as newer verson
+
+// alert( 3 + 3) // We are using node JS, not browser.
+
+// IMP : Code readability should be high. 
+
+console.log( 3 + 3 )
+
+console.log("Abhisek")
+
+let name = "Abhisek"
+let age = 20
+let isLoggedIn = false
+let state = null // or let state;
+
+
+// Number => 2 to the power 53
+// bigint
+// string => ""
+// boolian => true/false
+// nuLL => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Abhisek"); // string
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
+
+
