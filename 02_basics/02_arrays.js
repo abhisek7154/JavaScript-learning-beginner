bf2ca2a6ba06f63414bb0fa1960dfdd73e6fbab9
Abhisek sahoo <126792721+abhisek7154@users.noreply.github.com>
2025-03-17 13:58:04 +0000

    more on arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -12,3 +12,19 @@ console.log(mergedArray);
 const Spreadoperator = [...Dc_heros , ...Anime]
 console.log(Spreadoperator)
 
+const another_array = [1 , 2 , 3 , [ 4 , 5 , 6 ] , 7 , [ 6 , 7 , [ 4 , 5 ] ] ];
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+console.log(Array.isArray("Abhisek"))
+
+console.log(Array.from("Abhisek"))
+
+console.log(Array.from({name: "Abhisek"})) //Intresting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
