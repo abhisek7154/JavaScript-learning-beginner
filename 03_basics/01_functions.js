7c44ad5a2f644ca805c78c1dbd9032c4978a3fa6
Abhisek sahoo <126792721+abhisek7154@users.noreply.github.com>
2025-03-21 07:46:53 +0000

    functions 1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,19 @@
+// Functions
+ function sayMyName(){
+    console.log("A")
+    console.log("B")
+    console.log("H")
+    console.log("I")
+ }
+// call a function
+// sayMyName()
+
+function addTwoNumbers(number1 , number2) { // these are parameters 
+    console.log(number1 + number2);
+}
+
+//addTwoNumbers(3 , null) // these are arguments
+
+const result  = addTwoNumbers(3 ,5)  // 8 
+
+console.log("Result : " , result);  // Result :  undefined
